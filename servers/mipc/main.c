60e4e3c5080cc599fd99a132f9a1c38033118c01
Bo Feng <bfeng1216@gmail.com>
2012-02-26 00:36:05 +0000

    add mipc server

---
+++ servers/mipc/main.c
@@ -0,0 +1,93 @@
+/* 
+ * 
+ * Created:
+ *   2/24/2012  by Bo Feng
+ */
+
+#include <minix/endpoint.h>
+#include "inc.h"
+
+/* Allocate space for the global variables. */
+PRIVATE endpoint_t who_e;	/* caller's proc number */
+PRIVATE int callnr;		/* system call number */
+
+/*===========================================================================*
+ *				main                                         *
+ *===========================================================================*/
+PUBLIC int main(int argc, char **argv)
+{
+/* This is the main routine of this service. The main loop consists of 
+ * three major activities: getting new work, processing the work, and
+ * sending the reply. The loop never terminates, unless a panic occurs.
+ */
+  message m;
+  int result;                 
+  /* SEF local startup. */
+  env_setargs(argc, argv);
+  sef_local_startup();
+
+  puts("Hello, world");
+  /* Main loop - get work and do it, forever. */         
+  while(TRUE) {
+      /* Wait for incoming message, sets 'callnr' and 'who'. */
+      get_work(&m);
+
+      if (is_notify(callnr)) {
+          printf("DS: warning, got illegal notify from: %d\n", m.m_source);
+          result = EINVAL;
+          goto send_reply;
+      }
+
+      switch (callnr) {
+      default: 
+          printf("DS: warning, got illegal request from %d\n", m.m_source);
+          result = EINVAL;
+      }
+
+send_reply:
+      /* Finally send reply message, unless disabled. */
+      if (result != EDONTREPLY) {
+          m.m_type = result;  		/* build reply message */
+	  reply(who_e, &m);		/* send it away */
+      }
+  }
+  return(OK);				/* shouldn't come here */
+}
+
+/*===========================================================================*
+ *			       sef_local_startup			     *
+ *===========================================================================*/
+PRIVATE void sef_local_startup()
+{
+  /* No live update support for now. */
+
+  /* Let SEF perform startup. */
+  sef_startup();
+}
+
+/*===========================================================================*
+ *				get_work                                     *
+ *===========================================================================*/
+PRIVATE void get_work(
+  message *m_ptr			/* message buffer */
+)
+{
+    int status = sef_receive(ANY, m_ptr);   /* blocks until message arrives */
+    if (OK != status)
+        panic("failed to receive message!: %d", status);
+    who_e = m_ptr->m_source;        /* message arrived! set sender */
+    callnr = m_ptr->m_type;       /* set function call number */
+}
+
+/*===========================================================================*
+ *				reply					     *
+ *===========================================================================*/
+PRIVATE void reply(
+  endpoint_t who_e,			/* destination */
+  message *m_ptr			/* message buffer */
+)
+{
+    int s = send(who_e, m_ptr);    /* send the message */
+    if (OK != s)
+        printf("MIPC: unable to send reply to %d: %d\n", who_e, s);
+}
--- servers/rs/table.c
+++ servers/rs/table.c
@@ -25,6 +25,7 @@ PUBLIC struct boot_image_priv boot_image_priv_table[] = {
 {LOG_PROC_NR,  "log",    SRV_F  },
 {MFS_PROC_NR,"fs_imgrd", SRV_F  },
 {PFS_PROC_NR,  "pfs",    SRV_F  },
+{MIPC_PROC_NR, "mipc",   SRV_F  },
 {INIT_PROC_NR, "init",   USR_F  },
 {NULL_BOOT_NR, "",       0,     } /* null entry */
 };
