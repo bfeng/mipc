de6319aea5fa8ebe86ba893bef69e5fcb1444875
Bo Feng <bfeng1216@gmail.com>
2012-02-28 21:16:09 +0000

    add a manager for mipc and test files

---
+++ include/minix/mipc.h
@@ -0,0 +1,11 @@
+/* Prototypes and definitions for DS interface. */
+
+#ifndef _MINIX_MIPC_H
+#define _MINIX_MIPC_H
+
+#include <minix/types.h>
+#include <minix/endpoint.h>
+
+_PROTOTYPE( int mipc_create_grp, (void) );
+#endif
+
---
+++ test/mipc/mipc_test.c
@@ -0,0 +1,34 @@
+#include "inc.h"
+
+void test_create_grp(void)
+{
+  message dummy_msg;
+  _syscall(MIPC, MIPC_CREATE_GRP, &dummy_msg);
+}
+
+/* SEF functions and variables. */
+FORWARD _PROTOTYPE( void sef_local_startup, (void) );
+
+/*===========================================================================*
+ *				main					     *
+ *===========================================================================*/
+int main(void)
+{
+	/* SEF local startup. */
+	sef_local_startup();
+
+	/* Run all the tests. */
+        test_create_grp();
+	return 0;
+}
+
+
+/*===========================================================================*
+ *			       sef_local_startup			     *
+ *===========================================================================*/
+PRIVATE void sef_local_startup()
+{
+  /* Let SEF perform startup. */
+  sef_startup();
+}
+
